d4aaea436abc6cf6ac882a247d66964cea4acbad
Dasha Lary <dk2800@nyu.edu>
2020-09-10 23:38:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,5 @@
 def prime?(i)
-#   num = 2
-#   while i >= num
-# if i % num == 0 || i < 2 
-#   return false 
-#   num += 1 
-# else 
-#   return true 
-# end 
-# end 
-# end
+
 
 if i <= 1
 return false
